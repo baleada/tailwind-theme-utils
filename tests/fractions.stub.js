a2653a73b46e56de45826e734ccd271b6d73b071
AlexVipond <alex.r.vipond@gmail.com>
2020-04-13 15:27:38 -0600

    reduce all baleada fractions

--- tests/fractions.stub.js
+++ tests/fractions.stub.js
@@ -32,9 +32,9 @@ export const baleadaPercent = {
   '2/1': 'calc(2/1 * 100%)',
   '1/2': 'calc(1/2 * 100%)', '3/2': 'calc(3/2 * 100%)',
   '1/3': 'calc(1/3 * 100%)', '2/3': 'calc(2/3 * 100%)', '4/3': 'calc(4/3 * 100%)', '5/3': 'calc(5/3 * 100%)',
-  '1/4': 'calc(1/4 * 100%)', '2/4': 'calc(2/4 * 100%)', '3/4': 'calc(3/4 * 100%)', '5/4': 'calc(5/4 * 100%)', '7/4': 'calc(7/4 * 100%)',
+  '1/4': 'calc(1/4 * 100%)', '3/4': 'calc(3/4 * 100%)', '5/4': 'calc(5/4 * 100%)', '7/4': 'calc(7/4 * 100%)',
   '1/5': 'calc(1/5 * 100%)', '2/5': 'calc(2/5 * 100%)', '3/5': 'calc(3/5 * 100%)', '4/5': 'calc(4/5 * 100%)', '6/5': 'calc(6/5 * 100%)', '7/5': 'calc(7/5 * 100%)', '8/5': 'calc(8/5 * 100%)', '9/5': 'calc(9/5 * 100%)',
-  '1/6': 'calc(1/6 * 100%)', '2/6': 'calc(2/6 * 100%)', '3/6': 'calc(3/6 * 100%)', '4/6': 'calc(4/6 * 100%)', '5/6': 'calc(5/6 * 100%)', '7/6': 'calc(7/6 * 100%)', '11/6': 'calc(11/6 * 100%)',
+  '1/6': 'calc(1/6 * 100%)', '5/6': 'calc(5/6 * 100%)', '7/6': 'calc(7/6 * 100%)', '11/6': 'calc(11/6 * 100%)',
   full: '100%',
 }
 
@@ -42,9 +42,9 @@ export const baleadaViewportWidth = {
   'screen-2/1': 'calc(2/1 * 100vw)',
   'screen-1/2': 'calc(1/2 * 100vw)', 'screen-3/2': 'calc(3/2 * 100vw)',
   'screen-1/3': 'calc(1/3 * 100vw)', 'screen-2/3': 'calc(2/3 * 100vw)', 'screen-4/3': 'calc(4/3 * 100vw)', 'screen-5/3': 'calc(5/3 * 100vw)',
-  'screen-1/4': 'calc(1/4 * 100vw)', 'screen-2/4': 'calc(2/4 * 100vw)', 'screen-3/4': 'calc(3/4 * 100vw)', 'screen-5/4': 'calc(5/4 * 100vw)', 'screen-7/4': 'calc(7/4 * 100vw)',
+  'screen-1/4': 'calc(1/4 * 100vw)', 'screen-3/4': 'calc(3/4 * 100vw)', 'screen-5/4': 'calc(5/4 * 100vw)', 'screen-7/4': 'calc(7/4 * 100vw)',
   'screen-1/5': 'calc(1/5 * 100vw)', 'screen-2/5': 'calc(2/5 * 100vw)', 'screen-3/5': 'calc(3/5 * 100vw)', 'screen-4/5': 'calc(4/5 * 100vw)', 'screen-6/5': 'calc(6/5 * 100vw)', 'screen-7/5': 'calc(7/5 * 100vw)', 'screen-8/5': 'calc(8/5 * 100vw)', 'screen-9/5': 'calc(9/5 * 100vw)',
-  'screen-1/6': 'calc(1/6 * 100vw)', 'screen-2/6': 'calc(2/6 * 100vw)', 'screen-3/6': 'calc(3/6 * 100vw)', 'screen-4/6': 'calc(4/6 * 100vw)', 'screen-5/6': 'calc(5/6 * 100vw)', 'screen-7/6': 'calc(7/6 * 100vw)', 'screen-11/6': 'calc(11/6 * 100vw)',
+  'screen-1/6': 'calc(1/6 * 100vw)', 'screen-5/6': 'calc(5/6 * 100vw)', 'screen-7/6': 'calc(7/6 * 100vw)', 'screen-11/6': 'calc(11/6 * 100vw)',
   screen: '100vw',
 }
 
@@ -52,8 +52,8 @@ export const baleadaViewportHeight = {
   'screen-2/1': 'calc(2/1 * 100vh)',
   'screen-1/2': 'calc(1/2 * 100vh)', 'screen-3/2': 'calc(3/2 * 100vh)',
   'screen-1/3': 'calc(1/3 * 100vh)', 'screen-2/3': 'calc(2/3 * 100vh)', 'screen-4/3': 'calc(4/3 * 100vh)', 'screen-5/3': 'calc(5/3 * 100vh)',
-  'screen-1/4': 'calc(1/4 * 100vh)', 'screen-2/4': 'calc(2/4 * 100vh)', 'screen-3/4': 'calc(3/4 * 100vh)', 'screen-5/4': 'calc(5/4 * 100vh)', 'screen-7/4': 'calc(7/4 * 100vh)',
+  'screen-1/4': 'calc(1/4 * 100vh)', 'screen-3/4': 'calc(3/4 * 100vh)', 'screen-5/4': 'calc(5/4 * 100vh)', 'screen-7/4': 'calc(7/4 * 100vh)',
   'screen-1/5': 'calc(1/5 * 100vh)', 'screen-2/5': 'calc(2/5 * 100vh)', 'screen-3/5': 'calc(3/5 * 100vh)', 'screen-4/5': 'calc(4/5 * 100vh)', 'screen-6/5': 'calc(6/5 * 100vh)', 'screen-7/5': 'calc(7/5 * 100vh)', 'screen-8/5': 'calc(8/5 * 100vh)', 'screen-9/5': 'calc(9/5 * 100vh)',
-  'screen-1/6': 'calc(1/6 * 100vh)', 'screen-2/6': 'calc(2/6 * 100vh)', 'screen-3/6': 'calc(3/6 * 100vh)', 'screen-4/6': 'calc(4/6 * 100vh)', 'screen-5/6': 'calc(5/6 * 100vh)', 'screen-7/6': 'calc(7/6 * 100vh)', 'screen-11/6': 'calc(11/6 * 100vh)',
+  'screen-1/6': 'calc(1/6 * 100vh)', 'screen-5/6': 'calc(5/6 * 100vh)', 'screen-7/6': 'calc(7/6 * 100vh)', 'screen-11/6': 'calc(11/6 * 100vh)',
   screen: '100vh',
 }
